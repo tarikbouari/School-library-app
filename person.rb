3914a07c5ec7088e5cba2318908112a9d8c99eee
Rikstone <tarikbouari@gmail.com>
2022-11-10 10:06:30 +0000

    fix linters

--- person.rb
+++ person.rb
@@ -1,23 +1,21 @@
 class Person
-  def initialize(_name, age, _parent_pemission)
-    @id = Random.rand(1..1000)
-    @name = 'unknown'
-    @parent_pemission = true
-    @age = age
-  end
-  # add getters method
-  attr_accessor :name, :age
-  # add setters method
+  attr_accessor :age, :name
   attr_reader :id
 
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..9999)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
   def of_age?
-    if @age >= 18
+    @age >= 18
   end
 
   private :of_age?
 
   def can_use_services?
-      of_age?
+    of_age?
   end
-
 end
