b52a9a39436c1f16bec00db2bf018e3065151f64
Rikstone <tarikbouari@gmail.com>
2022-11-11 09:37:05 +0000

    Create classes

---
+++ nameable.rb
@@ -0,0 +1,29 @@
+class Nameable
+  def correct_name
+    raise NotImplementedError, 'Not Implmented'
+  end
+end
+
+class Decorator < Nameable
+  attr_accessor :name
+  def initialize(nameable)
+    @nameable = nameable
+    super()
+  end
+
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class CapitalizeDecoration < Decorator
+  def correct_name
+    @nameable.correct_name
+  end
+end
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.slice(0, 9)
+  end
+end
