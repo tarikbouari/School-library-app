885bff6e4dd78a1bc0dbb3cfc2a9a1209f921404
Rikstone <tarikbouari@gmail.com>
2022-11-17 12:43:21 +0000

    Group module files

---
+++ modules/mod_option.rb
@@ -0,0 +1,25 @@
+module EntryOption
+  def option # rubocop:disable Metrics/CyclomaticComplexity
+    puts 'Enter your choice: '
+    choice = gets.chomp.to_i
+
+    case choice
+    when 1
+      list_books(@books)
+    when 2
+      list_people(@people)
+    when 3
+      create_person(@people)
+    when 4
+      create_book(@books)
+    when 5
+      create_rental(@rentals, @people, @books)
+    when 6
+      list_rental(@rentals, @people)
+    when 7
+      exit
+    else
+      puts 'Invalid choice'
+    end
+  end
+end
