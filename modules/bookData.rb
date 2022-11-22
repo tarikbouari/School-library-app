cbe25530d954a1d71db334fbab49ae416d986869
Farida-Hamid <faridaaddala@gmail.com>
2022-11-22 16:39:00 +0300

    Write book to JSON file

--- modules/bookData.rb
+++ modules/bookData.rb
@@ -10,4 +10,8 @@ module BookData
     list.each |book| { store << {book.id, book.title, book.author} }
     JSON.generate(store)
   end
+
+  def write_books(file_name, list)
+    File.write(file_name, convert_list(list))
+  end
 end
