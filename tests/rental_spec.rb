9488482de58eaefc04676435857322e9fd594b80
Farida-Hamid <faridaaddala@gmail.com>
2022-11-24 10:58:45 +0300

    Test the rental class

---
+++ tests/rental_spec.rb
@@ -0,0 +1,21 @@
+require '../classes/rental'
+require '../classes/student'
+require '../classes/book'
+
+describe "Test the Rental class:" do
+  person = Student.new(21, name: "Sandy")
+  book = Book.new("Harry Potter", "J.K. Rowling")
+  rental = Rental.new("22/02/2022", person, book)
+
+  it "Chkec if the date is correct" do
+    expect(rental.date).to eql "22/02/2022"
+  end
+
+  it "Chkec if the date is correct" do
+    expect(rental.person.name).to eql "Sandy"
+  end
+
+  it "Chkec if the date is correct" do
+    expect(rental.book.title).to eql "Harry Potter"
+  end
+end
