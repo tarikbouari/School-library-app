c8b513e954e39abdfb6ba27826b4cc66c24137e1
[tarikbouari] <[tarikbouari@gmail.com]>
2022-11-24 10:16:34 +0000

    Create test for decorator class

--- tests/student_spec.rb
+++ tests/student_spec.rb
@@ -1,15 +1,15 @@
 require '../classes/student'
 
-describe "Test the Student class:" do
-  student = Student.new(12, name: "Sandy", parent_permission: false)
+describe 'Test the Student class:' do
+  student = Student.new(12, name: 'Sandy', parent_permission: false)
 
-  it "Check properties" do
-    expect(student.name).to eql "Sandy"
+  it 'Check properties' do
+    expect(student.name).to eql 'Sandy'
     expect(student.age).to eql 12
     expect(student.parent_permission).to eql false
   end
 
-  it "Check methode from inhereted class" do
+  it 'Check methode from inhereted class' do
     expect(student.can_use_services?).to eql false
   end
 end
--- tests/teacher_spec.rb
+++ tests/teacher_spec.rb
@@ -1,15 +1,15 @@
 require '../classes/teacher'
 
-describe "Test the teacher class:" do
-  student = Teacher.new("Math", 30, name: "Sam")
+describe 'Test the teacher class:' do
+  student = Teacher.new('Math', 30, name: 'Sam')
 
-  it "Check properties" do
-    expect(student.name).to eql "Sam"
+  it 'Check properties' do
+    expect(student.name).to eql 'Sam'
     expect(student.age).to eql 30
-    expect(student.specialization).to eql "Math"
+    expect(student.specialization).to eql 'Math'
   end
 
-  it "Check methode from inhereted class" do
+  it 'Check methode from inhereted class' do
     expect(student.can_use_services?).to eql true
   end
 end
